40994d45b5cd0dfa17538bbdbff5c75e74b01fad
Alex Levine <a17levine@gmail.com>
2016-01-24 13:57:31 -0600

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,12 @@
+//= require jquery
+//= require jquery_ujs
+//= require ember
+//= require ember-data
+//= require active-model-adapter
+//= require_self
+//= require Chart
+//= require ./gold
+
+// for more details see: http://emberjs.com/guides/application/
+Gold = Ember.Application.create();
+
---
+++ app/assets/javascripts/routes/example.js
@@ -0,0 +1,1 @@
+Gold.ExampleRoute = Ember.Route.extend();
